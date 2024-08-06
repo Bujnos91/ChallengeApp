8d344e761e6e3fb5d44ebd893b3da3d8e88aebfd
Marcin Bujnowski <marcinbujnowski91@gmail.com>
2024-08-06 19:34:18 +0100

    Tworzenie aplikacji

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Design;
+
+var name = "Marcin";
+var gender = 'M';
+var age = 33;
+
+if (gender == 'F' && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" && age == 30) 
+{
+    Console.WriteLine("Ewa lat 30)");
+}
+else if (gender == 'M' && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+else
+{ 
+    Console.WriteLine(); 
+}
